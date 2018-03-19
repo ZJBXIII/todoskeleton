d897f444252ea11b217a226456a496a9c866264d
XIII <zacharybrown1256@gmail.com>
2018-03-18 23:36:59 -0400

    t add . git commit
    	new file:   README.md

---
+++ main.java
@@ -0,0 +1,23 @@
+
+
+class main {
+
+
+public static void main(String[] args) {
+    
+
+
+System.out.println("Hello World");
+List<String> todoList  new List<String>();
+todoList.add("Item 1");
+todoList.add("Item 2");
+todoList.add("Item 3");
+todoList.add("Item 4");
+for (String todoList : item){
+    System.out.println(item);
+
+}
+
+    }
+
+}
